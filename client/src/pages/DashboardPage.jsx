6ddfcd2dcd5f8abea4d2f0a7ee30b27ac7df53e2
shreyasaboji <shreyasaboji1ga23is147@gmail.com>
2026-04-30 22:54:47 +0530

    replace all local api paths

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import { API_URL } from './config'
+import { API_URL } from '../config'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { API_URL } from './config'
+import { API_URL } from '../config'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
